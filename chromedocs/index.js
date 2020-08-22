4039ca851b8accba6bb6f93203946164ed2ec5b7
MurshidMac <mh.murshidco.uk@outlook.com>
2020-08-23 01:54:55 +0530

    Added the first screenshot script

---
+++ chromedocs/index.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+
+// Async Function to Just get a screenshot
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    await page.screenshot({path: 'google.png'})
+
+    await browser.close();
+})();   // Always call the function
