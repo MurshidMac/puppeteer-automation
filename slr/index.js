c3d570eb19e94466afb303ceb1292ca0bb81b8ba
MurshidMac <mh.murshidco.uk@outlook.com>
2020-08-25 16:19:06 +0530

    working changes

--- slr/index.js
+++ slr/index.js
@@ -1,16 +1,28 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    const page = await browser.newPage();
 
-  await page.goto('https://qa.its.nebula.slr-sandbox.co.uk/Identity/Account/Login');
+    await page.goto('https://dev.its.nebula.slr-sandbox.co.uk/Identity/Account/Login');
 
-  await page.type('#Input.Email', 'foo@example.com');
-  await page.type('#Input.Password', 'password');
-  await page.keyboard.press('Enter');
+    //await page.type('#Input.Email', 'foo@example.com');
+    //await page.type('#Input.Password', 'password');
+    await page.type('input[name="Input.Email"]', 'admin@email.com')
+    await page.type('input[name="Input.Password"]', 'Qwerty1@')
+    await page.keyboard.press('Enter');
 
-  await page.waitForNavigation();
-  console.log('New Page URL:', page.url());
-  await browser.close();
+    await page.waitForNavigation();
+    console.log('New Page URL:', page.url());
+
+    const linkHandlers = await page.$x("//a[contains(text(), 'Customers')]");
+
+    if (linkHandlers.length > 0) {
+        await linkHandlers[0].click();
+    } else {
+        throw new Error("Link not found");
+    }
+
+
+    //await browser.close();
 })();
